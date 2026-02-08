988e0c2305a4969441b6c17563d83dbbdeaeb0b8
Haseeb <rajahaseeb8090@gmail.com>
2026-02-09 00:01:01 +0500

    Initial Commit

--- diabetes_prediction.py
+++ diabetes_prediction.py
@@ -9,15 +9,16 @@ from sklearn.metrics import accuracy_score
 
 diabetes_dataset  = pd.read_csv(r'C:\Users\Skylink\Desktop\Diabetes Prediction\diabetes.csv')
 
-#pd.read_csv?
-
 # print(diabetes_dataset.head())
+
 
 #number of rows and columns in this dataset
 # print(diabetes_dataset.shape)
 
+
 #getting the statistical measures of the data
 # print(diabetes_dataset.describe())
+
 
 #total outcome values in this dataset
 # print(diabetes_dataset['Outcome'].value_counts())
@@ -27,12 +28,14 @@ diabetes_dataset  = pd.read_csv(r'C:\Users\Skylink\Desktop\Diabetes Prediction\d
 
 # print(diabetes_dataset.groupby('Outcome').mean())
 
+
 #separating the data and lables
 X = diabetes_dataset.drop(columns='Outcome', axis=1)
 Y = diabetes_dataset['Outcome']
 
 # print(X)
 # print(Y)
+
 
 #Data Standardization
 scaler = StandardScaler()
@@ -43,6 +46,54 @@ standardized_data = scaler.transform(X)
 X = standardized_data
 Y = diabetes_dataset['Outcome']
 
-print(X)
-print(Y)
+# print(X)
+# print(Y)
 
+
+#Train Test Split
+X_train, X_test, Y_train, Y_test = train_test_split(X, Y, test_size=0.2, stratify=Y, random_state=2)
+
+print(X.shape, X_train.shape, X_test.shape)
+
+
+#Training the model
+classifier = svm.SVC(kernel='linear')
+
+#training the support vector machine classifier
+classifier.fit(X_train, Y_train)
+
+
+#Model Evaluation 
+
+#accuracy score on training data
+X_train_prediction = classifier.predict(X_train)
+training_data_accuracy = accuracy_score(X_train_prediction, Y_train)
+print('Accuracy of the training data :', training_data_accuracy)
+
+#accuracy score on test data
+X_test_prediction = classifier.predict(X_test)
+test_data_accuracy = accuracy_score(X_test_prediction, Y_test)
+print('Accuracy of the test data :', test_data_accuracy) 
+
+
+#Making a Predictive System
+input_data = (5,166,72,19,175,25.8,0.587,51)
+
+#changing the input_data to numpy array
+input_data_as_np_array = np.asarray(input_data)
+
+#reshape the array as we are predicting as one instance
+input_data_reshaped = input_data_as_np_array.reshape(1, -1)
+
+#standardize the input data
+std_data = scaler.transform(input_data_reshaped)
+print(std_data)
+
+prediction = classifier.predict(std_data)
+print(prediction)
+
+if (prediction[0] == 0):
+    print('The person is not Diabetic.')
+
+else:
+    print('The person is Diabetic.')
